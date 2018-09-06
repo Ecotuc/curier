69af3f9be34245cf679062c3d1628665f57b83b3
ThelmaLemus <thelma.lemus@galileo.edu>
2018-09-05 23:26:15 -0600

    incompleto

---
+++ script_costos.sql
@@ -0,0 +1,78 @@
+-- Departamentos
+-- Alta Verapaz		Baja Verapáz
+-- Chimaltenango	Chiquimula
+-- El progreso		Escuintla
+-- Guatemala		Huehuetenango
+-- Izabal			Jalapa
+-- Jutiapa			Petén
+-- Quetzaltenango	Quiché
+-- Retahuleu		Sacatepequez
+-- San Marcos		Santa Rosa
+--Solola			Suchítepequez
+--Totonicapán		Zacapa
+
+-- Cobertura
+-- Guatemala
+-- Chimaltenango
+-- Sacatepequez
+-- Escuintla
+-- Retahuleu
+-- San Marcos
+-- Suchitepéquez
+
+
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Guatemala', 'Guatemala', , )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Guatemala', 'Chimaltenango', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Guatemala', 'Sacatepequez', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Guatemala', 'Escuintla', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Guatemala', 'Retahuleu', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Guatemala', 'San Marcos', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Guatemala', 'Suchitepéquez', )
+
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Chimaltenango', 'Guatemala', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Chimaltenango', 'Chimaltenango', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Chimaltenango', 'Sacatepequez', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Chimaltenango', 'Escuintla', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Chimaltenango', 'Retahuleu', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Chimaltenango', 'San Marcos', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Chimaltenango', 'Suchitepéquez', )
+
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Sacatepequez', 'Guatemala', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Sacatepequez', 'Chimaltenango', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Sacatepequez', 'Sacatepequez', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Sacatepequez', 'Escuintla', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Sacatepequez', 'Retahuleu', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Sacatepequez', 'San Marcos', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Sacatepequez', 'Suchitepéquez', )
+
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Escuintla', 'Guatemala', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Escuintla', 'Chimaltenango', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Escuintla', 'Sacatepequez', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Escuintla', 'Escuintla', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Escuintla', 'Retahuleu', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Escuintla', 'San Marcos', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Escuintla', 'Suchitepéquez', )
+
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Retahuleu', 'Guatemala', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Retahuleu', 'Chimaltenango', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Retahuleu', 'Sacatepequez', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Retahuleu', 'Escuintla', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Retahuleu', 'Retahuleu', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Retahuleu', 'San Marcos', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Retahuleu', 'Suchitepéquez', )
+
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('San Marcos', 'Guatemala', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('San Marcos', 'Chimaltenango', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('San Marcos', 'Sacatepequez', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('San Marcos', 'Escuintla', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('San Marcos', 'Retahuleu', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('San Marcos', 'San Marcos', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('San Marcos', 'Suchitepéquez', )
+
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Suchitepéquez', 'Guatemala', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Suchitepéquez', 'Chimaltenango', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Suchitepéquez', 'Sacatepequez', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Suchitepéquez', 'Escuintla', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Suchitepéquez', 'Retahuleu', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Suchitepéquez', 'San Marcos', )
+INSERT INTO costos(origen, destino, idd, precio) VALUES ('Suchitepéquez', 'Suchitepéquez', )
