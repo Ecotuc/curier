4f9f590b7a32cb93a284677a871b436c0b662a17
Edwin Cotuc <ivan.cotuc@gmail.com>
2018-09-13 10:31:49 -0600

    Avances en navbar y status bar
    Listo el tracking de los paquetes y la barra de navegación agregada en todos los archivos

--- php/costos.php
+++ php/costos.php
@@ -1,1 +1,10 @@
-costos.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DHL Guatemala | Ingreso de costos</title>
+	<?php include 'navbar.php' ?>
+</head>	
+<body>
+	
+</body>
+</html>
--- php/status.php
+++ php/status.php
@@ -5,71 +5,73 @@
 	if($varsesion == null || $varsesion == ''){
 		$varsesion= 'local';
 	}
+	$cualquiera= 1;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>DHL | Guatemala</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link href="../css/estilo.css" rel="stylesheet" type="text/css">
-	<link rel="icon" type="imgage/png" href="../images/tab_logo.png" sizes="32x32">
-</head>
-<body class="fondo">
-	<nav class="navbar navbar-inverse">
-	  <div class="container-fluid">
-	    <div class="navbar-header">
-	      <img class="nav_logo" src="../images/nav_logo.png">
+	<title>DHL Guatemala | Tracking</title>
+	<?php include "navbar.php" ?>
+<body class="fondo4">
+
+	<form class="form-register-tiendas"method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	  <div>
+	    <div class="col-lg-5 row">
+	      <input type="text" class="form-control" placeholder="Número de Orden" name="orden">
 	    </div>
-	    <ul class="nav navbar-nav">
-      		<?php
-				if(!($varsesion =='admin') && !($varsesion == 'local')){
-						echo "<li><a href=\"welcome.php\">Ingresar ordenes</a></li>
-						<li class=\"active\"><a href=\"cotizacion.php\">Cotización</a></li>
-						<li><a href=\"cerrarsesion.php\">Cerrar Sesión</a></li>";
-				
-				}elseif($varsesion =='admin'){
-						echo "<li><a href=\"welcome.php\">Ingresar ordenes</a></li>";
-						echo "<li><a href=\"tiendas.php\">Ingresar Tienda</a></li>
-          				<li><a href=\"costos.php\">Ingresar costos</a></li>
-          				<li class=\"active\"><a href=\"cotizacion.php\">Cotización</a></li>
-          				<li><a href=\"cerrarsesion.php\">Cerrar Sesión</a></li>";
-				}elseif ($varsesion == 'local') {
-					echo"<li><a href=\"index.html\">Home</a></li>
-	     			 <li class=\"active\"><a href=\"cotizacion.php\">Cotización</a></li>";
-				}
-			 ?>
-	    </ul>
+	    <div class="col-lg-2"></div>
+		<button type="submit" class=" btn btn-primary col-lg-5 col-md-2" name="submit">Rastrear pedido</button>
 	  </div>
-	</nav>
-	<form class="form_status"method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-		<label for="orden">Número de orden</label>
-		<input type="text" name="orden" placeholder="Ejemplo 11111">
-		<button type="submit" class="btn btn-primary col-md-5" name="submit">Rastrear pedido</button>
 	</form>
-	<center>
-	<?php 
-		$status = 0;
-		$order=$_POST['orden'];
-		if(isset($_POST['submit'])){
-		$dbconn = pg_connect("host=localhost dbname=curier user=postgres password=1998")
-			or die('Could not connect: ' . pg_last_error());
-		$query = "SELECT status FROM ordenes
-				  WHERE orden = '$order'";
-		$result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
-		$row = pg_fetch_row($result);
-		$status = $row[0];
-		echo "<br><br><br>El número de orden es: ", $order," <br><br><br>
-			  El status de la orden es: <h1>", $status, "</h1><br><br><br>";
+	<br><br><br><br><br><br>
+  	
+		<?php 
+			$status = 0;
+			$order=$_POST['orden'];
+			if(isset($_POST['submit'])){
+				$dbconn = pg_connect("host=localhost dbname=curier user=postgres password=1998")
+					or die('Could not connect: ' . pg_last_error());
+				$query = "SELECT status FROM ordenes
+						  WHERE orden = '$order'";
+				$result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+				$row = pg_fetch_row($result);
+				$status = $row[0];
+				echo "<div class=\"bp\">
+						<div class=\"progress col-lg-10 col-md-7 col-md-5\">";
+							switch ($status) {
+								case 1:
+									echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" style=\"width:20%\"><h5>Orden nueva</h5></div>";
+									break;
+								case 2:
+									echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" style=\"width:20%\"><h5>Orden nueva</h5></div>
+									<div class=\"progress-bar progress-bar-warning\" role=\"progressbar\" style=\"width:20%\"><h5>Surtiéndose</h5></div>";
+									break;
+								case 3:
+									echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" style=\"width:20%\"><h5>Orden nueva</h5></div>
+									<div class=\"progress-bar progress-bar-warning\" role=\"progressbar\" style=\"width:20%\"><h5>Surtiéndose</h5></div>
+									<div class=\"progress-bar progress-bar-danger\" role=\"progressbar\" style=\"width:20%\"><h5>Empacándose</h5></div>";
+									break;
+								case 4:
+									echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" style=\"width:20%\"><h5>Orden nueva</h5></div>
+									<div class=\"progress-bar progress-bar-warning\" role=\"progressbar\" style=\"width:20%\"><h5>Surtiéndose</h5></div>
+									<div class=\"progress-bar progress-bar-danger\" role=\"progressbar\" style=\"width:20%\"><h5>Empacándose</h5></div>
+									<div class=\"progress-bar progress-bar-route\" role=\"progressbar\" style=\"width:20%\"><h5>En ruta</h5></div>";
+									break;
+								case 5:
+									echo "<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" style=\"width:20%\"><h5>Orden nueva</h5></div>
+									<div class=\"progress-bar progress-bar-warning\" role=\"progressbar\" style=\"width:20%\"><h5>Surtiéndose</h5></div>
+									<div class=\"progress-bar progress-bar-danger\" role=\"progressbar\" style=\"width:20%\"><h5>Empacándose</h5></div>
+									<div class=\"progress-bar progress-bar-route\" role=\"progressbar\" style=\"width:20%\"><h5>En ruta</h5></div>
+									<div class=\"progress-bar progress-bar-success\" role=\"progressbar\" style=\"width:20%\"><h5>Entregada</h5></div>";
+									break;
+								default:
+									echo "<div class=\"progress-bar progress-bar-danger\" role=\"progressbar\" style=\"width:100%\"><h5>No existe ninguna orden asociada a este número de orden</h5></div>";
 
-		}
-
-	?>
-  	</center>
-
+							}
+				echo " </div>
+					 </div>";
+			}
+		?>	    
 
 
 
